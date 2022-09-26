d9617a06348e19b8b930c9e124ee76e279edd0ba
LVinnichenko <livinnichenko@gmail.com>
2022-09-26 00:09:04 -0400

    Ex5: Парсинг JSON

---
+++ src/test/java/LessonTwo.java
@@ -0,0 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+public class LessonTwo {
+
+    @Test
+    public void getJsonTest(){
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        String message_2 = response.get("messages.message[1]");
+        System.out.println(message_2);
+    }
+}
